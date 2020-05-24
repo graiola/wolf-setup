4ca78d1251e374844e3a2da3679a7923eeefe8d5
Gennaro Raiola <gennaro.raiola@gmail.com>
2020-05-24 16:38:11 -0700

    Add the possibility to spawn ruins and anymal

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 
+# These are fixed
+CONTAINER_NAME="wbc"
+IMAGE_NAME="wbc:latest"
+USAGE="Usage: ./run.sh [ROBOT_NAME=hyq|anymal] [WORLD_NAME=empty|ruins]\nExample: ./run.sh hyq ruins"
+
 # Check args
-if [ "$#" -ne 1 ]; then
-  echo "usage: ./run.sh [IMAGE_NAME=wbc:latest]"
-  IMAGE_NAME="wbc:latest"
-else
-  IMAGE_NAME=$1	
+if [[ ( $1 == "--help") ||  $1 == "-h" ]] 
+then 
+	echo -e $USAGE
+	exit 0
 fi
+if [ "$#" -lt 2 ]; then
+	WORLD_NAME=empty
+else
+	WORLD_NAME=$2
+fi
+if [ "$#" -lt 1 ]; then
+	ROBOT_NAME=hyq
+else
+	ROBOT_NAME=$1
+fi 
 
 # Get this script's path
 pushd `dirname $0` > /dev/null
@@ -18,7 +32,6 @@ set -e
 # Hacky
 xhost +local:docker
 
-CONTAINER_NAME="wbc"
 
 if [ `sudo systemctl is-active docker` = "inactive" ]; then
   echo "Docker inactive.  Starting docker..."
@@ -41,4 +54,4 @@ docker run --user `id -u`:sudo --hostname $HOSTNAME --device=/dev/dri:/dev/dri -
 --volume="$HOME/.ros:$HOME/.ros" \
 --volume="$HOME/.gazebo:$HOME/.gazebo" \
 --volume="$HOME/.ignition:$HOME/.ignition" \
--it $IMAGE_NAME $SHELL -c "eval export HOME=$HOME; cd $HOME; source /opt/ros/melodic/setup.bash; source /opt/ros/advr-superbuild/setup.bash; roslaunch wb_controller wb_controller_bringup.launch"
+-it $IMAGE_NAME $SHELL -c "eval export HOME=$HOME; cd $HOME; source /opt/ros/melodic/setup.bash; source /opt/ros/advr-superbuild/setup.bash; roslaunch wb_controller wb_controller_bringup.launch robot_name:=$ROBOT_NAME world_name:=$WORLD_NAME.world"

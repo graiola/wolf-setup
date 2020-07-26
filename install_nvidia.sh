f5afaffbb9e53e4594619f7d787db108d57ee68d
Serger <gennaro.raiola@gmail.com>
2020-07-25 18:41:55 -0700

    Add install_nvidia script, to test

---
+++ install_nvidia.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get install -y curl
+
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
+  sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
+sudo apt-get update
+
+sudo apt-get install -y nvidia-container-runtime libcuda-430

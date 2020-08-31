69593d4869137f729a179b8a64c49d6be893ef51
Gennaro Raiola <gennaro.raiola@jpl.nasa.gov>
2020-08-31 10:54:34 -0700

    Add nvidia-container-toolkit

--- install_nvidia.sh
+++ install_nvidia.sh
@@ -9,4 +9,4 @@ curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidi
   sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
 sudo apt-get update
 
-sudo apt-get install -y nvidia-container-runtime libcuda-430
+sudo apt-get install -y nvidia-container-runtime nvidia-container-toolkit libcuda-430

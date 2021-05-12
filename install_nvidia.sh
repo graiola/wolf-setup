0ef937fa687dd2f8e00d7280737b622c85af5d53
Gennaro Raiola <gennaro.raiola@gmail.com>
2021-05-12 16:58:11 +0200

    Fix install_nvidia

--- install_nvidia.sh
+++ install_nvidia.sh
@@ -9,6 +9,6 @@ curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidi
   sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
 sudo apt-get update
 
-sudo apt-get install -y nvidia-container-runtime nvidia-container-toolkit libcuda-430
+sudo apt-get install -y nvidia-container-runtime nvidia-container-toolkit nvidia-cuda-toolkit
 
 sudo systemctl restart docker

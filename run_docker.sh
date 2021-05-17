029ba0f8963c8642082f9ab7dcc56904060bb5e0
Gennaro Raiola <gennaro.raiola@gmail.com>
2021-05-17 15:13:21 +0200

    Better bash options

--- run_docker.sh
+++ run_docker.sh
@@ -1,26 +1,68 @@
 #!/usr/bin/env bash
 
-# These are fixed
+USAGE="Usage: \n run_docker [OPTIONS...] \n\nHelp Options:\n -h,--help \tShow help options\n\nApplication Options:\n -r \tRobot name (hyq,anymal)\n -w \tWorld name (empty,ruins)\n -a \tAdd the arm to the robot, available for hyq only"
+
+# Default
+ROBOT_NAME=hyq
+WORLD_NAME=empty
+ARM=false
+GUI=false
 CONTAINER_NAME="wbc"
 IMAGE_NAME="wbc:latest"
-USAGE="Usage: ./run_docker.sh [ROBOT_NAME=hyq|anymal] [WORLD_NAME=empty|ruins]\nExample: ./run_docker.sh hyq ruins"
 
-# Check args
 if [[ ( $1 == "--help") ||  $1 == "-h" ]] 
 then 
 	echo -e $USAGE
 	exit 0
 fi
-if [ "$#" -lt 2 ]; then
-	WORLD_NAME=empty
+
+while [ -n "$1" ]; do # while loop starts
+
+	case "$1" in
+
+        -r)
+		ROBOT_NAME="$2"
+		shift
+		;;
+
+	 -w)
+		WORLD_NAME="$2"
+		shift
+		;;
+
+	-a)    
+	        ARM=true
+		shift
+		;;
+
+	*) echo "Option $1 not recognized!" 
+		echo -e $USAGE
+		exit 0;;
+
+	esac
+
+	shift
+
+done
+
+# Checks
+if [[ ( $ROBOT_NAME == "hyq") ||  ( $ROBOT_NAME == "anymal") ]] 
+then 
+	echo "Selected robot: $ROBOT_NAME"
 else
-	WORLD_NAME=$2
+	echo "Wrong robot option!"
+	echo -e $USAGE
+	exit 0
 fi
-if [ "$#" -lt 1 ]; then
-	ROBOT_NAME=hyq
+
+if [[ ( $WORLD_NAME == "empty") ||  ( $WORLD_NAME == "ruins") ]] 
+then 
+	echo "Selected world: $WORLD_NAME"
 else
-	ROBOT_NAME=$1
-fi 
+	echo "Wrong world option!"
+	echo -e $USAGE
+	exit 0
+fi
 
 # Get this script's path
 pushd `dirname $0` > /dev/null
@@ -31,7 +73,6 @@ set -e
 
 # Hacky
 xhost +local:docker
-
 
 if [ `sudo systemctl is-active docker` = "inactive" ]; then
   echo "Docker inactive.  Starting docker..."
@@ -54,4 +95,4 @@ docker run --user `id -u`:sudo --hostname $HOSTNAME --device=/dev/dri:/dev/dri -
 --volume="$HOME/.ros:$HOME/.ros" \
 --volume="$HOME/.gazebo:$HOME/.gazebo" \
 --volume="$HOME/.ignition:$HOME/.ignition" \
--it $IMAGE_NAME $SHELL -c "eval export HOME=$HOME; cd $HOME; source /opt/ros/melodic/setup.bash; source /opt/ros/advr-superbuild/setup.bash; roslaunch wb_controller wb_controller_bringup.launch robot_name:=$ROBOT_NAME world_name:=$WORLD_NAME.world"
+-it $IMAGE_NAME $SHELL -c "eval export HOME=$HOME; cd $HOME; source /opt/ros/melodic/setup.bash; source /opt/ros/advr-superbuild/setup.bash; roslaunch wb_controller wb_controller_bringup.launch robot_name:=$ROBOT_NAME world_name:=$WORLD_NAME.world arm:=$ARM full_gui:=$GUI"

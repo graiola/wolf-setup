e56ac73c560f132bdfa40eb3a68c274dd81ed104
Gennaro Raiola <gennaro.raiola@gmail.com>
2023-01-10 13:37:39 +0100

    Test advr apt server

--- install.sh
+++ install.sh
@@ -105,18 +105,18 @@ then
 	sudo ldconfig
 	sudo rosdep init || true
 	rosdep update
-	#sudo sh -c 'echo "deb http://xbot.cloud/xbot2-nightly/ubuntu/$(lsb_release -sc) /" > /etc/apt/sources.list.d/xbot-nightly.list'
-	#wget -q -O - http://xbot.cloud/xbot2/ubuntu/KEY.gpg | sudo apt-key add -
-	#sudo apt-get update
-	#sudo apt-get install xbot2_desktop_full -y
+	sudo sh -c 'echo "deb http://xbot.cloud/xbot2-nightly/ubuntu/$(lsb_release -sc) /" > /etc/apt/sources.list.d/xbot-nightly.list'
+	wget -q -O - http://xbot.cloud/xbot2/ubuntu/KEY.gpg | sudo apt-key add -
+	sudo apt-get update
+	sudo apt-get install xbot2_desktop_full -y
 fi
 
 if [[ ( $INSTALL_OPT == "app") || ( $INSTALL_OPT == "all") ]]
 then 
 	# Download the debians
 	/bin/bash $SCRIPTPATH/support/get_debians.sh
-	echo -e "${COLOR_INFO}Install ADVR debian packages${COLOR_RESET}"
-	sudo $SCRIPTPATH/debs/$BRANCH_OPT/$UBUNTU/advr/install.sh
+	#echo -e "${COLOR_INFO}Install ADVR debian packages${COLOR_RESET}"
+	#sudo $SCRIPTPATH/debs/$BRANCH_OPT/$UBUNTU/advr/install.sh
 	echo -e "${COLOR_INFO}Install WoLF debian packages${COLOR_RESET}"
 	sudo dpkg -i --force-overwrite $SCRIPTPATH/debs/$BRANCH_OPT/$UBUNTU/*.deb
 fi

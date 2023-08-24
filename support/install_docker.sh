93b9c031789927fb7e064bb4e70445788aa7133f
graiola <gennaro.raiola@gmail.com>
2023-08-24 14:25:03 +0200

    Fix hardcoded arch in docker install

--- support/install_docker.sh
+++ support/install_docker.sh
@@ -3,7 +3,7 @@
 sudo apt-get install -y curl
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-compose
 

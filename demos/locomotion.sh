ae3bdc154a6c0a8423b15c2aebda93efcc673d70
Gennaro Raiola <gennaro.raiola@gmail.com>
2023-12-06 09:56:44 +0100

    Add planner in locomotion demo

--- demos/locomotion.sh
+++ demos/locomotion.sh
@@ -56,5 +56,5 @@ docker rm -f $CONTAINER_NAME > /dev/null 2>&1 || true
 docker run --user root:root --hostname $HOSTNAME --net=$NET --device=/dev/dri:/dev/dri --privileged -e "QT_X11_NO_MITSHM=1" -e GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/$ROS/share/wolf_gazebo_resources/models/ -e SHELL -e DISPLAY -e DOCKER=1 --name $CONTAINER_NAME \
         --gpus all \
         --device=/dev/ttyUSB0 \
-        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-        -it $IMAGE_NAME $SHELL -c "source /opt/ros/$ROS/setup.bash; source /opt/xbot/setup.sh; roslaunch wolf_controller wolf_controller_bringup.launch world_name:=$WORLD_NAME robot_model:=$ROBOT_MODEL robot_name:=$ROBOT_NAME rviz_gui:=true plot_node_gui:=true"
+        --volume="/tmp:/tmp:rw" \
+        -it $IMAGE_NAME $SHELL -c "source /opt/ros/$ROS/setup.bash; source /opt/xbot/setup.sh; source /opt/ocs2/setup.sh; roslaunch wolf_controller wolf_controller_bringup.launch world_name:=$WORLD_NAME robot_model:=$ROBOT_MODEL robot_name:=$ROBOT_NAME rviz_gui:=true plot_node_gui:=true launch_planner:=true"

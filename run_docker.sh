e75ffa98403af7e7a2c00e85e5273e67de8fa869
Gennaro Raiola <gennaro.raiola@gmail.com>
2025-01-08 09:04:06 +0100

    Update run_docker script

--- run_docker.sh
+++ run_docker.sh
@@ -9,25 +9,31 @@ set -e
 
 source $SCRIPTPATH/support/fun.cfg
 
-USAGE="Usage: \n run_docker [OPTIONS...] 
-\n\n
+# Help/Usage text with all options
+USAGE="Usage: run_docker [OPTIONS...]
+\n
 Help Options:
-\n 
--h,--help \tShow help options
-\n\n
+\n
+  -h, --help        Show help options
+\n
 Application Options:
-\n 
--r,--robot \tRobot model [spot|go1], example: -r spot
-\n 
--d,--device \tInput device type [ps3|xbox|twist|keyboard], example: -d ps3
-\n 
--w,--world \tWorld name [empty|ruins|pyramid|ramps|stairs|office], example: -w ruins
-\n 
--g,--gui \tLaunch rviz
-\n 
--n,--net \tLaunch docker with shared network, useful to visualize the ROS topics on the host machine
-\n 
--l,--local \tRun a local ROS workspace inside the container [workspace], example: -l ros_ws"
+\n
+  -r, --robot       Robot model [spot|go1], example: -r spot
+\n
+  -d, --device      Input device type [ps3|xbox|twist|keyboard], example: -d ps3
+\n
+  -w, --world       World name [empty|ruins|pyramid|ramps|stairs|office], example: -w ruins
+\n
+  -g, --gui         Launch rviz
+\n
+  -n, --net         Launch docker with shared network, useful to visualize the ROS topics on the host machine
+\n
+  -l, --local       Run a local ROS workspace inside the container [workspace], example: -l ros_ws
+\n
+  -i, --image       Specify the Docker image name [wolf-base|wolf-app], example: -i wolf-app
+\n
+  -t, --tag         Specify the Docker image tag [focal|jammy], example: -t focal
+"
 
 # Default
 ROBOT_NAME=
@@ -38,125 +44,164 @@ GUI=false
 RUN_LOCAL_WS=false
 DOCKER_NET=bridge
 ROS_WS=
-CONTAINER_NAME="wolf-app"
+IMAGE_NAME="wolf-app"
 IMAGE_TAG="focal"
+CONTAINER_NAME="wolf-container"
+DOCKER_REGISTRY=serger87
+
+function run_local_ros_workspace()
+{
+        docker run --user root:root --hostname $HOSTNAME --ipc=host --net=$DOCKER_NET --device=/dev/dri:/dev/dri --privileged -e "QT_X11_NO_MITSHM=1" -e GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/$2/share/wolf_gazebo_resources/models/ -e SHELL -e DISPLAY -e DOCKER=1 --name $CONTAINER_NAME \
+        --gpus all \
+        --device=/dev/ttyUSB0 \
+        --workdir="/home/$USER" \
+        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+        --volume="$HOME/$1:$HOME/$1" \
+        --volume="/etc/group:/etc/group:rw" \
+        --volume="/etc/passwd:/etc/passwd:rw" \
+        --volume="/etc/shadow:/etc/shadow:rw" \
+        --volume="/etc/sudoers:/etc/sudoers:rw" \
+        --volume="/etc/sudoers.d:/etc/sudoers.d:rw" \
+        --volume="$HOME/.ros:$HOME/.ros" \
+        --volume="$HOME/.gazebo:$HOME/.gazebo" \
+        --volume="$HOME/.ignition:$HOME/.ignition" \
+        --volume="$HOME/.rviz:$HOME/.rviz" \
+        -it $FULL_IMAGE_NAME $SHELL -c "eval export HOME=$HOME; cd $HOME; export XBOT_ROOT=/opt/ros/$1; source /opt/ros/$2/setup.bash; bash"
+}
+
+function run_docker_ros_workspace()
+{
+        docker run --user root:root --hostname $HOSTNAME --ipc=host --net=$DOCKER_NET --device=/dev/dri:/dev/dri --privileged -e "QT_X11_NO_MITSHM=1" -e GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/$1/share/wolf_gazebo_resources/models/ -e SHELL -e DISPLAY -e DOCKER=1 --name $CONTAINER_NAME \
+        --gpus all \
+        --device=/dev/ttyUSB0 \
+        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+        -it $FULL_IMAGE_NAME $SHELL -c "export XBOT_ROOT=/opt/ros/$1; source /opt/ros/$1/setup.bash; roslaunch wolf_controller wolf_controller_bringup.launch robot_model:=$ROBOT_MODEL world_name:=$WORLD_NAME full_gui:=$GUI input_device:=$DEVICE"
+}
 
 wolf_banner
 
-if [[ ( $1 == "--help") ||  $1 == "-h" ]] 
-then 
-	echo -e $USAGE
-	exit 0
+if [[ ( $1 == "--help") ||  $1 == "-h" ]]
+then
+        echo -e $USAGE
+        exit 0
 fi
 
 while [ -n "$1" ]; do # while loop starts
 
-	case "$1" in
+        case "$1" in
 
-	-r|--robot)
-		ROBOT_MODEL="$2"
-		shift
-		;;
+        -r|--robot)
+                ROBOT_MODEL="$2"
+                shift
+                ;;
 
- 	-d|--device)
-		DEVICE="$2"
-		shift
-		;;
+        -d|--device)
+                DEVICE="$2"
+                shift
+                ;;
 
-	-w|--world)
-		WORLD_NAME="$2"
-		shift
-		;;
+        -w|--world)
+                WORLD_NAME="$2"
+                shift
+                ;;
 
-	-g|--gui)    
-		GUI=true
-		;;
+        -g|--gui)
+                GUI=true
+                ;;
 
-	-n|--net)    
-		DOCKER_NET=host
-		;;
+        -n|--net)
+                DOCKER_NET=host
+                ;;
 
-	-l|--local)    
-		ROS_WS="$2"
-		RUN_LOCAL_WS=true
-		shift
-		;;
+        -l|--local)
+                ROS_WS="$2"
+                RUN_LOCAL_WS=true
+                shift
+                ;;
 
-	*) print_warn "Option $1 not recognized!" 
-		echo -e $USAGE
-		exit 0;;
+        -i|--image)
+                IMAGE_NAME="$2"
+                shift
+                ;;
 
-	esac
+        -t|--tag)
+                IMAGE_TAG="$2"
+                shift
+                ;;
 
-	shift
+        *) print_warn "Option $1 not recognized!"
+                echo -e $USAGE
+                exit 0;;
+
+        esac
+
+        shift
 done
 
-# Checks
-if [[ ( $ROBOT_MODEL == "spot") ||  ( $ROBOT_MODEL == "go1")]] 
-then 
-	print_info "Selected robot: $ROBOT_MODEL"
-else
-	print_warn "Wrong robot model option!"
-	echo -e $USAGE
-	exit 0
+# Validating options
+valid_robots=("spot" "go1")
+valid_devices=("ps3" "xbox" "twist" "keyboard")
+valid_worlds=("empty" "ruins" "pyramid" "ramps" "stairs" "office")
+valid_tags=("focal" "jammy")
+
+if [[ ! " ${valid_robots[*]} " =~ " $ROBOT_MODEL " ]]; then
+    print_warn "Invalid robot model!"
+    echo -e "$USAGE"
+    exit 1
 fi
 
-if [[ ( $DEVICE == "ps3") ||  ( $DEVICE == "xbox") ||  ( $DEVICE == "twist") ||  ( $DEVICE == "keyboard") ]] 
-then 
-	print_info "Selected input device: $DEVICE"
-else
-	print_warn "Wrong input device option!"
-	echo -e $USAGE
-	exit 0
+if [[ ! " ${valid_devices[*]} " =~ " $DEVICE " ]]; then
+    print_warn "Invalid input device!"
+    echo -e "$USAGE"
+    exit 1
 fi
 
-if [[ ( $WORLD_NAME == "empty") ||  ( $WORLD_NAME == "ruins") ||  ( $WORLD_NAME == "pyramid") ||  ( $WORLD_NAME == "ramps") ||  ( $WORLD_NAME == "stairs") ||  ( $WORLD_NAME == "office") ]] 
-then 
-	print_info "Selected world: $WORLD_NAME"
-else
-	print_warn "Wrong world option!"
-	echo -e $USAGE
-	exit 0
+if [[ ! " ${valid_worlds[*]} " =~ " $WORLD_NAME " ]]; then
+    print_warn "Invalid world name!"
+    echo -e "$USAGE"
+    exit 1
 fi
 
-
-if [[ ( $IMAGE_TAG == "focal") || ( $IMAGE_TAG == "jammy") ]]
-then
-	ROS=noetic
-else
-	print_warn "Wrong image option!"
-	echo -e $USAGE
-	exit 0
+if [[ ! " ${valid_tags[*]} " =~ " $IMAGE_TAG " ]]; then
+    print_warn "Invalid image tag!"
+    echo -e "$USAGE"
+    exit 1
+elif [[ $IMAGE_TAG == "focal" ]]; then
+    ROS=noetic
+elif [[ $IMAGE_TAG == "jammy" ]]; then
+    ROS=humble
 fi
 
-# Define the image name
-IMAGE_NAME=serger87/$CONTAINER_NAME:$IMAGE_TAG
+# Define the full Docker image name
+FULL_IMAGE_NAME="$IMAGE_NAME:$IMAGE_TAG"
 
-# Hacky
+# Ensure Docker is running
+if ! sudo systemctl is-active --quiet docker; then
+    echo "Docker inactive. Starting docker..."
+    sudo systemctl restart docker
+fi
+
+# Pull Docker image if not present
+if ! docker image inspect "$FULL_IMAGE_NAME" > /dev/null 2>&1; then
+    print_warn "Image $FULL_IMAGE_NAME not found. Pulling from registry..."
+    docker pull "$DOCKER_REGISTRY/$FULL_IMAGE_NAME"
+    docker tag "$DOCKER_REGISTRY/$FULL_IMAGE_NAME" "$FULL_IMAGE_NAME"
+fi
+
+# Cleanup existing Docker container if necessary
+docker rm -f "$CONTAINER_NAME" > /dev/null 2>&1 || true
+
+# xhost settings to allow Docker GUI access
 xhost +local:docker
-
-if [ `sudo systemctl is-active docker` = "inactive" ]; then
-  echo "Docker inactive. Starting docker..."
-  sudo systemctl start docker
-fi
-
-# Cleanup the docker container before launching it
-docker rm -f $CONTAINER_NAME > /dev/null 2>&1 || true 
 
 # Run the container with shared X11
 # Opt1 run the code within the docker container by sourcing the local ROS workspace (useful for development)
 # Opt2 run the code within the docker container by sourcing the ROS workspace INSIDE docker (useful as a demo)
 if $RUN_LOCAL_WS;
-then 
-	if [ -f "$HOME/$ROS_WS/devel/setup.bash" ];
-	then
-		print_info "Selected ros workspace: $ROS_WS"
-		run_local_ros_workspace $ROS_WS $ROS
-	else
-		print_warn "The file $HOME/$ROS_WS/devel/setup.bash does not exist!"
-		echo -e $USAGE
-		exit 0
-	fi
+then
+        print_info "Selected ros workspace: $ROS_WS"
+        run_local_ros_workspace $ROS_WS $ROS
+
 else
-	run_docker_ros_workspace $ROS
+        run_docker_ros_workspace $ROS
 fi
